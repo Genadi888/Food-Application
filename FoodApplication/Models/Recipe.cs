9f79b62b431a2a21a765d27bc81d8f0699d67562
Stefan-Mitev <123822245+Stefan-Mitev@users.noreply.github.com>
2024-03-13 18:18:03 +0200

    Home page , Recipes

---
+++ FoodApplication/Models/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodApplication.Models
+{
+    public class Recipe
+    {
+        public string id { get; set; }
+        public string Image_url { get; set; }
+        public string Publisher { get; set; }
+        public string Title { get; set; }
+    }
+}
--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let apiURL = "https://forkify-api.herokuapp.com/api/v2/recipes"
+let apikey = "4ab73696-d61f-465b-9336-7f9ad55b259e"
+
+async function GetRecipes(recipeName,id,isAllShow) {
+    let resp = await fetch(`${apikey}?search=${recipeName}&key=${apikey}`);
+    let result = await resp.json();
+    console.log(result);
+    let Recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(0, 5)
+    showRecipes(showRecipes, id);
+}
+function showRecipes(recipes, id) {
+    $.ajax({
+        contentType: "application/json; charset=utf-8",
+        html: 'html',
+        type: 'POST',
+        url: '/Recipe/GetRecipeCard',
+        data: JSON.stringify(recipes),
+        success: function (htmlResult) {
+            $('#' + id).html(htmlResult);
+        }
+        })
+}
