1a189eeeb5405f3fbc263749825d7a492521a316
Genadi888 <genadi.fidanov@abv.bg>
2024-03-14 20:02:44 +0200

    did some basic things about the orders

---
+++ FoodApplication/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodApplication.Models;
+
+namespace FoodApplication.Controllers
+{
+	public class RecipeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult GetRecipeCard([FromBody] List<Recipe> recipes)
+		{
+			return PartialView("_RecipeCard", recipes);
+		}
+
+		public IActionResult Search([FromQuery] string recipe)
+		{
+			ViewBag.Recipe = recipe;
+			return View();
+		}
+
+		public IActionResult Order([FromQuery] string id) 
+		{
+			ViewBag.Id = id;
+			return View();
+		}
+	}
+}
--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const apiURL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+const apiKey = "b4a57a58-feb4-4e70-8015-f45242e63c38"
+
+
+async function GetRecipes(recipeName, id, isAllShow) {
+    let resp = await fetch(`${apiURL}?search=${recipeName}&key=${apiKey}`);
+    let result = await resp.json();
+    let recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(1, 7);
+    showRecipes(recipes, id);
+}
+
+function showRecipes(recipes, id) {
+    $.ajax({
+        contentType: 'application/json; charset=utf-8',
+        datatype: "html",
+        type: 'POST',
+        url: '/Recipe/GetRecipeCard',
+        data: JSON.stringify(recipes),
+        success: function (htmlResult) {
+            $('#' + id).html(htmlResult);
+        }
+    })
+}
+
+async function getOrderRecipe(id) {
+    let resp = await fetch(`${apiURL}/${id}?key=${apiKey}`);
+    let result = await resp.json();
+    console.log(result);
+}
