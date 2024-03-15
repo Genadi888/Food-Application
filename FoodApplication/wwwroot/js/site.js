b60194c44d5b20e2951bb202fd8ba7ae54851e49
Genadi888 <genadi.fidanov@abv.bg>
2024-03-15 21:34:59 +0200

    did some big things - had the recipes show up

--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -10,6 +10,7 @@ const apiKey = "b4a57a58-feb4-4e70-8015-f45242e63c38"
 async function GetRecipes(recipeName, id, isAllShow) {
     let resp = await fetch(`${apiURL}?search=${recipeName}&key=${apiKey}`);
     let result = await resp.json();
+    //console.log(result);
     let recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(1, 7);
     showRecipes(recipes, id);
 }
@@ -22,6 +23,7 @@ function showRecipes(recipes, id) {
         url: '/Recipe/GetRecipeCard',
         data: JSON.stringify(recipes),
         success: function (htmlResult) {
+            console.log(id);
             $('#' + id).html(htmlResult);
         }
     })
