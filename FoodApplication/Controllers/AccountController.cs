ed17f8057894acc821227ec34b2440b9a3f7a22f
Genadi888 <genadi.fidanov@abv.bg>
2024-03-16 19:12:34 +0200

    added login and register

---
+++ FoodApplication/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApplication.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
